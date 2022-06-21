f0e4eeb7116badf7befb1047983feadffecb89dd
[prasanna] <prasanna>
2022-06-21 22:38:39 +0530

    [prasanna]: create a payroll service

---
+++ ADO.NETPayRoll/EmployeePayRoll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NETPayRoll
+{
+    internal class EmployeePayRoll
+    {
+        public static string dbpath = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=employ_PayRoll;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(dbpath);
+        //Fuction to check DB connection Establishment
+        public void DatabseConnection()
+        {
+            try
+            {
+                connect.Open();
+                using (connect)
+                {
+                    Console.WriteLine("Database connectivity successful.");
+                }
+                connect.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Database connectivity failed");
+            }
+        }
+
+    }
+}
+
---
+++ ADO.NETPayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ADO.NETPayRoll
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            ADO.NETPayRoll.EmployeePayRoll value = new ADO.NETPayRoll.EmployeePayRoll();
+            value.DatabseConnection();
+        }
+    }
+}
